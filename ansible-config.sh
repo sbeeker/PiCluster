0dce80b9597b192ad5fb7739355b987aacb8b100
Scott A. Beeker <sbeeker@ix.netcom.com>
2024-03-21 02:56:02 -0400

    Updated scripts.

--- ansible-config.sh
+++ ansible-config.sh
@@ -11,20 +11,26 @@ ifconfig br0
 
 # Configure the PPA and install Ansible run these commands:
 sudo apt update
-# these seem to cause issues
-# sudo apt install software-properties-common
-# sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
-# Check installed ansible version (In my case it is 2.9.6)
+
+# Check installed ansible version 
 ansible --version 
 
-# Configure to skip the host key check in the file /etc/ansible/ansible.cfg file and add the following part:
+# Configure to skip the host key check in the file /etc/ansible/ansible.cfg 
+# file and add the following part
 # [defaults]
- host_key_checking = False
+#  host_key_checking = False
+touch ansible.cfg
+echo "host_key_checking = False" >> ansible.cfg
+echo "ansible_python_interpreter = /usr/bin/python" >> ansible.cfg
+echo "interpreter_python = /usr/bin/python" >> ansible.cfg
 
 # test the connectivity
-ansible myservers -i myhosts -m ping -u pi --ask-pass
+ansible servers -i myhosts -m ping -u pi --ask-pass
 
-# As of Ansible 2.9, shell completion of the Ansible command line utilities is available and provided through an optional dependency called argcomplete
+# As of Ansible 2.9, shell completion of the Ansible command line utilities 
+# is available and provided through an optional dependency called argcomplete
 sudo apt install python-argcomplete
 
